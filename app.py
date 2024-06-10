0c6cdce2254d00e404fbc50352acba52039337ce
pws-pm <147876953+pws-pm@users.noreply.github.com>
2024-06-10 19:38:14 +0200

    delta e method

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import json
-from colormath.color_objects import sRGBColor, LabColor, LCHabColor
+from colormath.color_objects import sRGBColor, LabColor
 from colormath.color_conversions import convert_color
 import numpy as np
 import os
@@ -20,35 +20,68 @@ with open(ral_classic_path, 'r') as file:
 with open(ral_design_path, 'r') as file:
     ral_design_colors = json.load(file)
 
-def hex_to_oklch(hex_color):
+def hex_to_lab(hex_color):
     rgb = sRGBColor.new_from_rgb_hex(hex_color)
     lab = convert_color(rgb, LabColor)
-    lch = convert_color(lab, LCHabColor)
-    return (lch.lch_l, lch.lch_c, lch.lch_h)
+    return lab
 
-def calculate_oklch_values(ral_colors):
-    oklch_values = {}
+def calculate_lab_values(ral_colors):
+    lab_values = {}
     for ral_name, ral_hex in ral_colors.items():
-        oklch_values[ral_name] = hex_to_oklch(ral_hex)
-    return oklch_values
+        lab_values[ral_name] = hex_to_lab(ral_hex)
+    return lab_values
 
-# Precompute OKLCH values for RAL Classic and RAL Design colors
+# Precompute Lab values for RAL Classic and RAL Design colors
 @st.cache_data
-def get_precomputed_oklch():
+def get_precomputed_lab():
     return {
-        'classic': calculate_oklch_values(ral_classic_colors),
-        'design': calculate_oklch_values(ral_design_colors)
+        'classic': calculate_lab_values(ral_classic_colors),
+        'design': calculate_lab_values(ral_design_colors)
     }
 
-precomputed_oklch = get_precomputed_oklch()
+precomputed_lab = get_precomputed_lab()
 
-def color_difference(oklch1, oklch2):
-    return np.sqrt(sum((a - b) ** 2 for a, b in zip(oklch1, oklch2)))
+def delta_e_cie2000(lab1, lab2):
+    # CIEDE2000 formula implementation
+    L1, a1, b1 = lab1.lab_l, lab1.lab_a, lab1.lab_b
+    L2, a2, b2 = lab2.lab_l, lab2.lab_a, lab2.lab_b
+    
+    # Mean values
+    L_ = (L1 + L2) / 2
+    C1 = np.sqrt(a1**2 + b1**2)
+    C2 = np.sqrt(a2**2 + b2**2)
+    C_ = (C1 + C2) / 2
+    G = 0.5 * (1 - np.sqrt(C_**7 / (C_**7 + 25**7)))
+    
+    a1_ = (1 + G) * a1
+    a2_ = (1 + G) * a2
+    C1_ = np.sqrt(a1_**2 + b1**2)
+    C2_ = np.sqrt(a2_**2 + b2**2)
+    C_ = (C1_ + C2_) / 2
+    
+    h1 = np.degrees(np.arctan2(b1, a1_)) % 360
+    h2 = np.degrees(np.arctan2(b2, a2_)) % 360
+    H_ = (h1 + h2) / 2 if np.abs(h1 - h2) <= 180 else (h1 + h2 + 360) / 2 if h1 + h2 < 360 else (h1 + h2 - 360) / 2
+    
+    T = 1 - 0.17 * np.cos(np.radians(H_ - 30)) + 0.24 * np.cos(np.radians(2 * H_)) + 0.32 * np.cos(np.radians(3 * H_ + 6)) - 0.20 * np.cos(np.radians(4 * H_ - 63))
+    dH = h2 - h1 if np.abs(h2 - h1) <= 180 else h2 - h1 + 360 if h2 <= h1 else h2 - h1 - 360
+    dL_ = L2 - L1
+    dC_ = C2_ - C1_
+    dH_ = 2 * np.sqrt(C1_ * C2_) * np.sin(np.radians(dH / 2))
+    
+    S_L = 1 + (0.015 * (L_ - 50)**2) / np.sqrt(20 + (L_ - 50)**2)
+    S_C = 1 + 0.045 * C_
+    S_H = 1 + 0.015 * C_ * T
+    R_T = -2 * np.sqrt(C_**7 / (C_**7 + 25**7)) * np.sin(np.radians(60 * np.exp(-((H_ - 275) / 25)**2)))
+    
+    dE = np.sqrt((dL_ / S_L)**2 + (dC_ / S_C)**2 + (dH_ / S_H)**2 + R_T * (dC_ / S_C) * (dH_ / S_H))
+    
+    return dE
 
-def find_closest_ral_colors(input_oklch, ral_oklch, num_matches):
+def find_closest_ral_colors(input_lab, ral_lab, num_matches):
     differences = []
-    for ral_name, ral_oklch_values in ral_oklch.items():
-        diff = color_difference(input_oklch, ral_oklch_values)
+    for ral_name, ral_lab_values in ral_lab.items():
+        diff = delta_e_cie2000(input_lab, ral_lab_values)
         differences.append((diff, ral_name))
     
     differences.sort(key=lambda x: x[0])
@@ -56,20 +89,20 @@ def find_closest_ral_colors(input_oklch, ral_oklch, num_matches):
 
 st.title('Hex to RAL Color Matcher')
 
-hex_color = st.text_input('Enter a HEX color (e.g., #34a8eb):', '#34a8eb')
+hex_color = st.text_input('Enter a HEX color (e.g., #2e2f33):', '#2e2f33')
 palette_type = st.selectbox('Select RAL Palette', ['RAL Classic', 'RAL Design'])
 num_matches = st.selectbox('Number of matches', list(range(1, 10)), index=8)
 
 if palette_type == 'RAL Classic':
     ral_palette = ral_classic_colors
-    ral_oklch = precomputed_oklch['classic']
+    ral_lab = precomputed_lab['classic']
 else:
     ral_palette = ral_design_colors
-    ral_oklch = precomputed_oklch['design']
+    ral_lab = precomputed_lab['design']
 
 if hex_color:
-    input_oklch = hex_to_oklch(hex_color)
-    closest_colors = find_closest_ral_colors(input_oklch, ral_oklch, num_matches)
+    input_lab = hex_to_lab(hex_color)
+    closest_colors = find_closest_ral_colors(input_lab, ral_lab, num_matches)
     
     # Display the input color
     st.markdown(f"<div style='text-align:center; padding:10px; font-size:24px;'>Input Color: {hex_color}</div>", unsafe_allow_html=True)
